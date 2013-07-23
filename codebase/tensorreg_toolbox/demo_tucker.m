41733fb548f3f9fae92d9ccf9402259f119642bb
Hua Zhou <hua_zhou@ncsu.edu>
2013-07-22 21:16:33 -0400

    break demo_tucker.m into more blocks for better readability

--- codebase/tensorreg_toolbox/demo_tucker.m
+++ codebase/tensorreg_toolbox/demo_tucker.m
@@ -5,58 +5,71 @@ clear;
 s = RandStream('mt19937ar','Seed',2);
 RandStream.setGlobalStream(s);
 
-% 2D true signal 64-by-64: cross
+%%
+% True coefficients for regular (non-array) covariates
+p0 = 5;
+b0 = ones(p0,1);
+
+%%
+% 2D true signal: 64-by-64 cross
 shape = imread('cross.gif'); 
 shape = imresize(shape,[32,32]); % 32-by-32
 b = zeros(2*size(shape));
 b((size(b,1)/4):(size(b,1)/4)+size(shape,1)-1, ...
     (size(b,2)/4):(size(b,2)/4)+size(shape,2)-1) = shape;
 [p1,p2] = size(b);
-% true coefficients for regular (non-array) covariates
-p0 = 5;
-b0 = ones(p0,1);
+display(size(b));
 
-% simulate covariates
+%%
+% Simulate covariates
 n = 500;    % sample size
 X = randn(n,p0);   % n-by-p0 regular design matrix
 M = tensor(randn(p1,p2,n));  % p1-by-p2-by-n matrix variates
-% the systematic part
+display(size(M));
+
+%%
+% Simulate responses
 mu = X*b0 + double(ttt(tensor(b), M, 1:2));
-% simulate responses
 sigma = 1;  % noise level
 y = mu + sigma*randn(n,1);
 
-% estimate using Tucker linear regression - rank (1 1)
+%%
+% Estimate using Tucker linear regression - rank (1 1)
 tic;
 disp('rank (1 1)');
 [~,beta_rk1,glmstats1] = tucker_reg(X,M,y,[1 1],'normal');
 toc;
 
-% estimate using Tucker linear regression - rank (1 2)
+%%
+% Estimate using Tucker linear regression - rank (1 2)
 tic;
 disp('rank (1 2)');
 [~,beta_rk12,glmstats12] = tucker_reg(X,M,y,[1 2],'normal');
 toc;
 
-% estimate using Tucker linear regression - rank (2 2)
+%%
+% Estimate using Tucker linear regression - rank (2 2)
 tic;
 disp('rank (2 2)');
 [~,beta_rk2,glmstats2] = tucker_reg(X,M,y,[2 2],'normal');
 toc;
 
-% estimate using Tucker linear regression - rank (2 3)
+%%
+% Estimate using Tucker linear regression - rank (2 3)
 tic;
 disp('rank (2 3)');
 [~,beta_rk23,glmstats23] = tucker_reg(X,M,y,[2 3],'normal');
 toc;
 
-% estimate using Tucker linear regression - rank (3 3)
+%%
+% Estimate using Tucker linear regression - rank (3 3)
 tic;
 disp('rank (3 3)');
 [~,beta_rk3,glmstats3] = tucker_reg(X,M,y,[3 3],'normal');
 toc;
 
-% display true and recovered signals
+%%
+% Display true and recovered signals
 figure; hold on;
 set(gca,'FontSize',20);
 
@@ -102,35 +115,40 @@ title({['rank=(3,3), ', ' BIC=',num2str(glmstats3{end}.BIC,5)]});
 axis equal;
 axis tight;
 
-%% sparse Tucker linear regression, 2D covariates
+%% Sparse Tucker linear regression, 2D covariates
 
-% set lasso penalty and tuning parameter values
+%%
+% Set lasso penalty and tuning parameter values
 pentype = 'enet';
 penparam = 1;
 lambda = [1,100,1000];
 
-% estimate using Tucker sparse linear regression - lambda 1
+%%
+% Estimate using Tucker sparse linear regression - lambda 1
 tic;
 disp(['lambda=', num2str(lambda(1))]);
 [~,beta_rk1,~,glmstat_rk1] = tucker_sparsereg(X,M,y,3,'normal',...
     lambda(1),pentype,penparam);
 toc;
 
-% estimate using Tucker sparse linear regression - lambda 2
+%%
+% Estimate using Tucker sparse linear regression - lambda 2
 tic;
 disp(['lambda=', num2str(lambda(2))]);
 [~,beta_rk2,~,glmstat_rk2] = tucker_sparsereg(X,M,y,3,'normal',...
     lambda(2),pentype,penparam);
 toc;
 
-% estimate using Tucker sparse linear regression - lambda 3
+%%
+% Estimate using Tucker sparse linear regression - lambda 3
 tic;
 disp(['lambda=', num2str(lambda(3))]);
 [~,beta_rk3,~,glmstat_rk3] = tucker_sparsereg(X,M,y,3,'normal',...
     lambda(3),pentype,penparam);
 toc;
 
-% display true and recovered signals
+%%
+% Display true and recovered signals
 figure; hold on;
 set(gca,'FontSize',20);
 
@@ -175,7 +193,8 @@ clear;
 s = RandStream('mt19937ar','Seed',2);
 RandStream.setGlobalStream(s);
 
-% 2D true signal 64-by-64: cross
+%%
+% 2D true signal: 64-by-64 cross
 shape = imread('cross.gif'); 
 shape = imresize(shape,[32,32]); % 32-by-32
 b = zeros(2*size(shape));
@@ -186,46 +205,55 @@ b((size(b,1)/4):(size(b,1)/4)+size(shape,1)-1, ...
 p0 = 5;
 b0 = ones(p0,1);
 
-% simulate covariates
+%%
+% Simulate covariates
 n = 1000;    % sample size
 X = randn(n,p0);   % n-by-p regular design matrix
 M = tensor(randn(p1,p2,n));  % p1-by-p2-by-n matrix variates
-% the systematic part
+display(size(M));
+
+%%
+% Simulate binoary responses from the systematic components
 mu = X*b0 + double(ttt(tensor(b), M, 1:2));
-% simulate binoary responses from the systematic components
 y = binornd(1, 1./(1+exp(-mu)));
 
-% estimate using Tucker logistic regression - rank (1 1)
+%%
+% Estimate using Tucker logistic regression - rank (1 1)
 tic;
 disp('rank (1 1)');
 [beta0_rk1,beta_rk1,glmstats1,dev1] = tucker_reg(X,M,y,[1 1],'binomial');
 toc;
 
-% estimate using Tucker logistic regression - rank (1 2)
+%%
+% Estimate using Tucker logistic regression - rank (1 2)
 tic;
 disp('rank (1 2)');
 [~,beta_rk12,glmstats12] = tucker_reg(X,M,y,[1 2],'binomial');
 toc;
 
-% estimate using Tucker logistic regression - rank (2 2)
+%%
+% Estimate using Tucker logistic regression - rank (2 2)
 tic;
 disp('rank (2 2)');
 [~,beta_rk2,glmstats2] = tucker_reg(X,M,y,[2 2],'binomial');
 toc;
 
-% estimate using Tucker logistic regression - rank (2 3)
+%%
+% Estimate using Tucker logistic regression - rank (2 3)
 tic;
 disp('rank (2 3)');
 [~,beta_rk23,glmstats23] = tucker_reg(X,M,y,[2 3],'binomial');
 toc;
 
-% estimate using Tucker logistic regression - rank (3 3)
+%%
+% Estimate using Tucker logistic regression - rank (3 3)
 tic;
 disp('rank (3 3)');
 [~,beta_rk3,glmstats3] = tucker_reg(X,M,y,[3 3],'binomial');
 toc;
 
-% display true and recovered signals
+%%
+% Display true and recovered signals
 figure; hold on;
 set(gca,'FontSize',20);
 
@@ -271,35 +299,40 @@ title({['rank=[3 3], ', ' BIC=',num2str(glmstats3{end}.BIC,5)]});
 axis equal;
 axis tight;
 
-%% sparse Tucker logistic regression, 2D covariates
+%% Sparse Tucker logistic regression, 2D covariates
 
-% set lasso penalty and tuning parameter values
+%%
+% Set lasso penalty and tuning parameter values
 pentype = 'enet';
 penparam = 1;
 lambda = [1,10,200];
 
-% estimate using Tucker sparse logistic regression - lambda 1
+%%
+% Estimate using Tucker sparse logistic regression - lambda 1
 tic;
 disp(['lambda=', num2str(lambda(1))]);
 [~,beta_rk1,~,glmstat_rk1] = tucker_sparsereg(X,M,y,3,'binomial',...
     lambda(1),pentype,penparam);
 toc;
 
-% estimate using Tucker sparse logistic regression - lambda 2
+%%
+% Estimate using Tucker sparse logistic regression - lambda 2
 tic;
 disp(['lambda=', num2str(lambda(2))]);
 [~,beta_rk2,~,glmstat_rk2] = tucker_sparsereg(X,M,y,3,'binomial',...
     lambda(2),pentype,penparam);
 toc;
 
-% estimate using Tucker sparse logistic regression - lambda 3
+%%
+% Estimate using Tucker sparse logistic regression - lambda 3
 tic;
 disp(['lambda=', num2str(lambda(3))]);
 [~,beta_rk3,~,glmstat_rk3] = tucker_sparsereg(X,M,y,3,'binomial',...
     lambda(3),pentype,penparam);
 toc;
 
-% display true and recovered signals
+%%
+% Display true and recovered signals
 figure; hold on;
 set(gca,'FontSize',20);
 
@@ -344,16 +377,20 @@ clear;
 s = RandStream('mt19937ar','Seed',2);
 RandStream.setGlobalStream(s);
 
-% true 3D signal: simple "two-cube"
+%%
+% True coefficients for regular covariates
+p0 = 5;
+b0 = ones(p0,1);
+
+%%
+% True 3D signal: 25-by-25-by-25 "two-cube"
 b = zeros(25,25,25);
 b(6:10,6:10,6:10) = 1;
 b(18:22,18:22,8:22) = 1;
 [p1, p2, p3] = size(b);
-% true coefficients for regular covariates
-p0 = 5;
-b0 = ones(p0,1);
 
-% simulate covariates
+%%
+% Simulate covariates
 n = 500;    % sample size
 X = randn(n,p0);   % n-by-p regular design matrix
 M = tensor(randn(p1,p2,p3,n));  % p1-by-p2-by-p3 3D variates
@@ -363,39 +400,43 @@ mu = X*b0 + double(ttt(M,tensor(b),1:3));
 sigma = 1;  % noise level
 y = mu + sigma*randn(n,1);
 
-% estimate by Tucker linear regression - rank (1 1 1)
+%%
+% Estimate by Tucker linear regression - rank (1 1 1)
 tic;
 disp('rank (1 1 1)');
 [~,beta_rk1,glmstats1] = tucker_reg(X,M,y,[1 1 1],'normal');
 toc;
 
-% estimate by Tucker linear regression - rank (1 2 2)
+%%
+% Estimate by Tucker linear regression - rank (1 2 2)
 tic;
 disp('rank (1 2 2)');
 [~,beta_rk122,glmstats122] = tucker_reg(X,M,y,[1 2 2],'normal');
 toc;
 
-
-% estimate by Tucker linear regression - rank (2 2 2)
+%%
+% Estimate by Tucker linear regression - rank (2 2 2)
 tic;
 disp('rank (2 2 2)');
 [~,beta_rk2,glmstats2] = tucker_reg(X,M,y,[2 2 2],'normal');
 toc;
 
-% estimate by Tucker linear regression - rank (2 3 3)
+%%
+% Estimate by Tucker linear regression - rank (2 3 3)
 tic;
 disp('rank (2 3 3)');
 [~,beta_rk233,glmstats233] = tucker_reg(X,M,y,[2 3 3],'normal');
 toc;
 
-
-% estimate by Tucker linear regression - rank (3 3 3)
+%%
+% Estimate by Tucker linear regression - rank (3 3 3)
 tic;
 disp('rank (3 3 3)');
 [~,beta_rk3,glmstats3] = tucker_reg(X,M,y,[3 3 3],'normal');
 toc;
 
-% display true and recovered signals
+%%
+% Display true and recovered signals
 figure; hold on;
 set(gca,'FontSize',20);
 
@@ -454,35 +495,40 @@ zlim([1 p3]);
 title({['rank=(3,3,3), ', ' BIC=', num2str(glmstats3{end}.BIC,5)]});
 axis equal;
 
-%% sparse Tucker linear regression, 3D covaraites, 
+%% Sparse Tucker linear regression, 3D covaraites, 
 
-% set lasso penalty and tuning parameter values
+%%
+% Set lasso penalty and tuning parameter values
 pentype = 'enet';
 penparam = 1;
 lambda = [1,100,1000];
 
-% estimate using Tucker sparse linear regression - lambda 1
+%%
+% Estimate using Tucker sparse linear regression - lambda 1
 tic;
 disp(['lambda=', num2str(lambda(1))]);
 [~,beta_rk1,~,glmstat_rk1] = tucker_sparsereg(X,M,y,3,'normal',...
     lambda(1),pentype,penparam);
 toc;
 
-% estimate using Tucker sparse linear regression - lambda 2
+%%
+% Estimate using Tucker sparse linear regression - lambda 2
 tic;
 disp(['lambda=', num2str(lambda(2))]);
 [~,beta_rk2,~,glmstat_rk2] = tucker_sparsereg(X,M,y,3,'normal',...
     lambda(2),pentype,penparam);
 toc;
 
-% estimate using Tucker sparse linear regression - lambda 3
+%%
+% Estimate using Tucker sparse linear regression - lambda 3
 tic;
 disp(['lambda=', num2str(lambda(3))]);
 [~,beta_rk3,~,glmstat_rk3] = tucker_sparsereg(X,M,y,3,'normal',...
     lambda(3),pentype,penparam);
 toc;
 
-% display true and recovered signals
+%%
+% Display true and recovered signals
 figure; hold on;
 set(gca,'FontSize',20);
 
